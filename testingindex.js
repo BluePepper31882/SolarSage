8e83fae8914e19d9200b785c306886416261d682
BluePepper31882 <acwolk00@gmail.com>
2023-12-13 22:12:16 -0500

    me and aidan cooked again

--- testingindex.js
+++ testingindex.js
@@ -1,23 +1,61 @@
 
 var x = 0;
-var timer;
+var y = 0;
+var z = 0; 
+var counter;
+var counter2;
+var counter3;
 
 
 
-timer = setInterval(addToo, 3);
+counter = setInterval(addToo1, 6);
+counter2 = setInterval(addToo2, 6);
+counter3 = setInterval(addToo3, 1000);
+
+
+
 checkX = setInterval(checkX, 1);
+checkY = setInterval(checkY, 1);
+checkZ = setInterval(checkZ, 1);
 
  
 function checkX(){
-    if(x >= 1000){
-        clearInterval(timer);
-        console.log("Cleared Timer");
+    if(x >= 3276){
+        clearInterval(counter);
+        clearInterval(checkX);
     }
 }
 
-function addToo(){
-        x+=1;
-        document.getElementById("demo").innerHTML = x;
+
+function checkY(){
+    if(y >= 1478){
+        clearInterval(counter2);
+
+    }
+} 
+
+
+
+
+
+
+function addToo1(){
+        x+=3;
+        document.getElementById("demo1").innerHTML = x;
     
+} 
+
+
+function addToo2(){
+    y+=3;
+    document.getElementById("demo2").innerHTML = y;
+
+} 
+
+
+function addToo3(){
+    z+=.05;
+    document.getElementById("demo3").innerHTML = z.toFixed(2);
+
 }
 
